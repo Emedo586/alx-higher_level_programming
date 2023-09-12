41047b4c468be576d9e80dcffd6d8ad4f859eeeb
Emedo586 <emedopeace99@gmail.com>
2023-09-12 02:13:32 +0000

    Rewards for grit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include "Python.h"
+#include <Python.h>
 
 /**
  * print_python_list_info - Prints basic info about python lists.
@@ -18,7 +18,7 @@ printf("[*] Allocated = %d\n", alloc);
 for (i = 0; i < size; i++)
 {
 printf("Element %d: ", i);
-obj = Pylist_GetItem(p, i);
-printf{"%s\n", Py_TYPE(obj)->tp_name};
+obj = PyList_GetItem(p, i);
+printf("%s\n", Py_TYPE(obj)->tp_name);
 }
 }
