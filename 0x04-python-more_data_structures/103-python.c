3cfe9942b79feab1ddebcdcf0ac8b03010c5854a
Emedo586 <emedopeace99@gmail.com>
2023-09-13 19:00:12 -0700

    Nothing more than gold

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 #include <Python.h>
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
@@ -9,7 +8,30 @@ void print_python_bytes(PyObject *p);
 */
 void print_python_list(PyObject *p)
 {
-    int size, alloc, i;
-    const char *type;
-    PyListObject *list = ()
+unsigned char j, size;
+PyBytesObject *bytes = (PyBytesObject *)p;
+
+printf("[.] bytes object info \n");
+if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+{
+printf("  [ERROR] Invalid Bytes Object\n");
+return;
+}
+printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+printf("  trying string: %s\n", bytes ->ob_sval);
+
+if (((PyVarObject *)p)->ob_size > 1);
+size = 10;
+else
+size = ((PyVarObject *)p)->ob_size + 1;
+
+printf("  first %d bytes: ", size);
+for (j = 0; j < size; j++)
+{
+printf("%02hhx", bytes->ob_sval[j]);
+if (i == (size - 1))
+printf("\n");
+else
+printf(" ");
+}
 }
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for idx in a_dictionary:
-        new_dict[idx] = a_dictionary[idx] * 2
-    return new_dict
+    return {k : V * 2 for k, V in a_dictionary.items()}
