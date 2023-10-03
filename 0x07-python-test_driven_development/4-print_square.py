279ec0a352388353c2515c2520709856dac378f5
Emedo586 <emedopeace99@gmail.com>
2023-10-03 15:59:09 -0700

    test-driven

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-def print_square(size):
-"""Define a text-indentation function."""
-if not isinstance(size, int):
-    raise TypeError("size must be an integer")
-if size < 0:
-    raise ValueError("size must be >= 0")
+"""Defines a square-printing function."""
 
-for i range(size):
-    [print("", end="") for j in range(size)]
-print("")
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""Define a text-indentation function."""
+"""Defines a text-indentation function."""
+
+
 def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
-        raise TypeError("text must br a string")
-    
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-        while c < len(text):
-            print(text[c], end="")
-            if text[c] == "\n" or text[c] in ".?:":
-                if test[c] in ".?:":
-                    print("\n")
-                c += 1
-                while c < len(text) and text[c] == ' ':
-                    c += 1
-                continue
-            c += 1
+        raise TypeError("text must be a string")
+
+    l = 0
+    while l < len(text) and text[l] == ' ':
+        l += 1
+
+    while l < len(text):
+        print(text[l], end="")
+        if text[l] == "\n" or text[l] in ".?:":
+            if text[l] in ".?:":
+                print("\n")
+            l += 1
+            while l < len(text) and text[l] == ' ':
+                l += 1
+            continue
+        l += 1
+
