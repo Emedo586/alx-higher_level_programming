3b4eb185e24ede92c55ea0ff5deec261ee296c4e
Emedo586 <emedopeace99@gmail.com>
2023-10-09 15:41:05 -0700

    WISE WORDS ONLY

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of Rectangle instance."""
+        """Return the print() and str() representation of new Rectangle."""
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
